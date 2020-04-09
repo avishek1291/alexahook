ae9f2b25e2be665c19ff1c73b22b296bbea9fc1b
Avishek <choudhury.avishek1@tcs.com>
2020-04-09 21:31:38 +0530

    code refactor

--- lambda/custom/requestHelper.js
+++ lambda/custom/requestHelper.js
@@ -3,6 +3,7 @@ const axios  =  require('axios');
 const app_id = 'AbpMlscxuETlDHg91GGV';
 const app_code='UYMXPafUbKIDewS_23tY7g';
 const token  = '11384968a33eb9cefd66bdd04b77da59c1'
+const jenkinsUrl = 'http://localhost:8080/job/DemoJob/build?token='
 const requestHelper =  {};
  const getLocation =  async (suggestionKey) => {
     console.log('revived suggesttion key', suggestionKey)
@@ -10,7 +11,7 @@ const requestHelper =  {};
       //   const mapUri =  `https://places.cit.api.here.com/places/v1/autosuggest
       //   ?at=22.572645,88.363892`
       //   const suggestions  =  await axios.get(mapUri+`&q=${suggestionKey}&app_id=${app_id}&app_code=${app_code}`);
-       await axios.get(`http://localhost:8080/job/DemoJob/build?token=${token}`);
+       await axios.get(`jenkinsUrl${token}`);
         return `build started for ${suggestionKey}`;
      }catch(e){
         console.log('error response', e)
